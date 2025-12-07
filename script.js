8b6f2029aa0b780d6bcb1e6f2df567c9f4193df3
SamStrange7 <63666828+SamStrange77@users.noreply.github.com>
2025-12-07 20:25:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let limit = 3801600
 
 console.log('VERSION 1: FIXED SECONDS');
-console.log('VERSION 3: MIO!! COLOR AND NEG');
+console.log('VERSION 3: MIO!! COLOR AND NEG fix');
 
 let timer = setInterval
 (
     () => 
     {   
-        let milliseconds = Math.floor(new Date("21-01-2026") - new Date());
+        let milliseconds = Math.floor(new Date("01-21-2026") - new Date());
         let seconds = Math.floor(milliseconds/1000);
         let minutes = Math.floor(seconds/60);
         let hours = Math.floor(minutes/60);
@@ -15,7 +15,7 @@ let timer = setInterval
         milliseconds*=-1;
         milliseconds%=1000;
         console.log(seconds);
-        document.body.color = rgb(Math.ceil((Math.abs(limit-seconds)/limit) * 200), Math.floor((seconds/limit) * 200), 0);
+        document.body.style.setProperty('background-color', `rgb(${Math.ceil((Math.abs(limit-seconds)/limit) * 200)}, ${Math.floor((seconds/limit) * 200)}, 18)`)    
         console.log(`rgb(${Math.ceil((Math.abs(limit-seconds)/limit) * 200)}, ${Math.floor((seconds/limit) * 200)}, 0)`);
         document.getElementById('seconds').innerHTML =
         `
@@ -67,6 +67,7 @@ function normalised_ms (a)
 {
     return a<100 ? a<10 ? '00'+ a : '0'+a : a;
 }
+
 
 
 
